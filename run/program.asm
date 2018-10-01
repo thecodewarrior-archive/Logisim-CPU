b0c65b263b9949c570d618ff7cb722e30d041c86
Pierce Corcoran <pierce@plasticcow.com>
2018-09-30 17:20:31 -0700

    Optimize circuits

--- run/program.asm
+++ run/program.asm
@@ -4,9 +4,11 @@ STORE ALU INC REG_A
 STORE CONST 1 REG_B
 test:
     STORE ALU_SWAP INC REG_B
-    JMP_IF ALU CMP_EQ >display
-    JMP_IF ALU_EQZ REM >check_next
+    JMP_IF ALU_EQZ REM >is_prime
 JMP >test
+
+is_prime: JMP_IF ALU CMP_EQ >display
+JMP >check_next
 display:
 DISPLAY REG_A
 JMP >check_next

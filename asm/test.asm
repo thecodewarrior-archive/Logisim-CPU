73fa6141afa401d44850fc488b1cfa33869c2f48
Pierce Corcoran <pierce@plasticcow.com>
2018-10-09 22:59:41 -0700

    Speed up prime calculation

--- asm/test.asm
+++ asm/test.asm
@@ -1,4 +1,6 @@
+display = R0
 possiblePrime = R1
+halfPossiblePrime = R4
 i = R2
 checkAgainst = R3
 
@@ -8,12 +10,14 @@ ld ~possiblePrime, 1
 checkNextPrime:
     ld ~i, 0
     calc ~possiblePrime, add, ~possiblePrime, 2
+    calc ~halfPossiblePrime, shl, ~possiblePrime, 1
     test:
         ld ~checkAgainst, *~i
-        jmpif foundPrime, eqz calc add, ~checkAgainst, 0
+        jmpif foundPrime, nez calc cmp_geq, ~checkAgainst, ~halfPossiblePrime
         jmpif checkNextPrime, eqz calc mod, ~possiblePrime, ~checkAgainst
         calc ~i, add, ~i, 1
         jmp test
     foundPrime:
         ld *~i, ~possiblePrime
+        calc ~display, add, ~possiblePrime, 0
         jmp checkNextPrime
